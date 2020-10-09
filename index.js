988cc144ef7f3fc7c0b780cb0be58c86e6f7052d
baaaam771 <jeongiun@naver.com>
2020-10-10 04:29:00 +0900

    classList & toggle

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const title = document.querySelector("#title");
 
 const CLICKED_CLASS = "clicked";
-
+/*
 function handleClick() {
-  const currentClass = title.className;
-  if (currentClass !== CLICKED_CLASS) {
-    title.className = CLICKED_CLASS;
-    console.log("!==");
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if (hasClass) {
+    title.classList.remove(CLICKED_CLASS);
+    console.log(title.classList);
   } else {
-    title.className = "";
-    console.log("===");
+    title.classList.add(CLICKED_CLASS);
+    console.log(title.classList);
   }
+}
+*/
+//위에 함수랑 똑같은 기능
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
