43717fe7ced0995e79200b43befb7ecef5581bf6
baaaam771 <jeongiun@naver.com>
2020-10-13 21:24:19 +0900

    python

--- index.py
+++ index.py
@@ -1,24 +1,34 @@
 class Calculator:
-def __init__(self, number, by_what):
+    def __init__(self, number, by_what):
 
+        self.number = number
+        self.by_what = by_what
 
-self.number = number
-self.by_what = by_what
+    def plus(self):
 
+        plus = self.number + self.by_what
+        return plus
 
-def plus(self):
+    def minus(self):
 
+        minus = self.number - self.by_what
+        return minus
 
-plus = self.number + self.by_what
-return plus
-
-
-def minus(self):
-
-
-minus = self.number - self.by_what
-return minus
 
 number = Calculator(2, 5)
 print(number.plus())
 print(number.minus())
+
+# ctrl /
+
+
+def say_hello(name, age):
+    return f"Hello {name} you are {age} years old"
+    # f= format
+    # return "Hello " + name + " you are" + age + " years old"
+
+
+hello = say_hello("nico", "12")
+# hello = say_hello(age="12", name="nico")
+# 많은 인자가 생기면 순서를 기억하지 못하게 된다. 그래서 keyword argument를 활용함
+print(hello)
